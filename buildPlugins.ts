5f47ab0ccbe841e1c833a91028ff600bb382b8a5
deniso4ka <den.zagrosh@gmail.com>
2022-10-02 17:16:30 +0300

    Updates

--- buildPlugins.ts
+++
@@ -1,6 +0,0 @@
-import webpack from 'webpack'
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-export function buildPlugins(): webpack.WebpackPluginInstance[] {
-   return [new HtmlWebpackPlugin({ template: './public/index.html' })]
-}
